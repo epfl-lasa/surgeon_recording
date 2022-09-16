a19dd0c7e97038059a8606ca09bc9c931e2e1376
Anaelle Manon <anaelle@lesmanon.com>
2022-09-16 17:40:30 +0200

    angular velocity metric, removed nan values for integration

---
+++ source/surgeon_recording/surgeon_recording/functions_metrics.py
@@ -0,0 +1,35 @@
+import math
+ 
+    
+    
+class F_metrics(object):
+    def __init__(self):
+        self.test = [0]
+
+    
+    def euler_from_quaternion(self, quaternion ):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        x = quaternion[0]
+        y = quaternion[1]
+        z = quaternion[2]
+        w= quaternion[3]
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
