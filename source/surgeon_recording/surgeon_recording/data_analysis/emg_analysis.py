6240fd52cfc00f5b9a9af7c6a84bf9d063436baf
cecilecbs <cecile.cabasse@gmail.com>
2023-01-27 18:18:30 +0100

    attempt sample entropy

--- source/surgeon_recording/surgeon_recording/data_analysis/emg_analysis.py
+++ source/surgeon_recording/surgeon_recording/data_analysis/emg_analysis.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.signal as sp
 import pyemgpipeline as pep
 from scipy.fft import fft
+import EntropyHub as EH
 import numpy as np
+import entropy as ent
 from matplotlib.figure import SubplotParams
 import os 
 import pywt
@@ -118,32 +120,38 @@ w4 = myfct.lowpassfilter(cleanemgDF[label_studied], thresh = 0.05, wavelet="db4"
 w6 = myfct.lowpassfilter(cleanemgDF[label_studied], thresh = 0.05, wavelet="db6")
 w20 = myfct.lowpassfilter(cleanemgDF[label_studied], thresh = 0.05, wavelet="db20")
 
-fig, axs = plt.subplots(5, 1, sharex='col')
-axs[0].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w1, label='db4' )
-axs[1].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w2, label='db2' )
-axs[2].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w4, label='db4' )
-axs[3].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w6, label='db6' )
-axs[4].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w20, label='db20' )
+# fig, axs = plt.subplots(5, 1, sharex='col')
+# axs[0].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w1, label='db4' )
+# axs[1].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w2, label='db2' )
+# axs[2].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w4, label='db4' )
+# axs[3].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w6, label='db6' )
+# axs[4].plot(cleanemgDF["relative time"], cleanemgDF[label_studied], cleanemgDF["relative time"], w20, label='db20' )
 
-axs[0].set_ylabel('cleanemgDF and db1 (mV)')
-axs[1].set_ylabel('cleanemgDF and db2 (mV)')
-axs[2].set_ylabel('cleanemgDF and db4 (mV)')
-axs[3].set_ylabel('cleanemgDF and db6 (mV)')
-axs[4].set_ylabel('cleanemgDF and db20 (mV)')
+# axs[0].set_ylabel('cleanemgDF and db1 (mV)')
+# axs[1].set_ylabel('cleanemgDF and db2 (mV)')
+# axs[2].set_ylabel('cleanemgDF and db4 (mV)')
+# axs[3].set_ylabel('cleanemgDF and db6 (mV)')
+# axs[4].set_ylabel('cleanemgDF and db20 (mV)')
 
-fig.tight_layout()
-plt.subplots_adjust(top=0.95,
-                    bottom=0.04,
-                    left=0.055,
-                    right=0.995,
-                    hspace=0.4,
-                    wspace=0.2)
-plt.xlim([0, cleanemgDF['relative time'].iloc[-1]])
-axs.legend()
-axs.set_title('Removing High Frequency Noise with DWT', fontsize=18)
-axs[4].set_xlabel('Time')
-plt.show()
-# Best = bd4
+# fig.tight_layout()
+# plt.subplots_adjust(top=0.95,
+#                     bottom=0.04,
+#                     left=0.055,
+#                     right=0.995,
+#                     hspace=0.4,
+#                     wspace=0.2)
+# plt.xlim([0, cleanemgDF['relative time'].iloc[-1]])
+# axs.legend()
+# axs.set_title('Removing High Frequency Noise with DWT', fontsize=18)
+# axs[4].set_xlabel('Time')
+# plt.show()
+# # Best = bd4
+
+
+#-SAMPLE ENTROPY
+std = np.std(cleanemgDF[label_studied])
+# SampEnDF,_ = EH.SampEn(cleanemgDF[label_studied], m=2, r = 0.2 * std) 
+SampEnDF = ent.sample_entropy(cleanemgDF[label_studied])
 
 #-PYEMGPIPELINE
 # mgr = pep.wrappers.DataProcessingManager()
