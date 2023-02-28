b4565732a4662e7f9373f49c38e40078e6b76ded
cecilecbs <cecile.cabasse@gmail.com>
2023-02-28 16:33:47 +0100

    try to plot in bars

--- source/surgeon_recording/surgeon_recording/data_analysis/main_emg_analysis_test_features.py
+++ source/surgeon_recording/surgeon_recording/data_analysis/main_emg_analysis_test_features.py
@@ -2,6 +2,7 @@ from modules import emg_utils as myfct
 from modules.emg_analysis_test_features import Emg_analysis_features
 import matplotlib.pyplot as plt
 import pandas as pd
+import textwrap
 
 cecile = Emg_analysis_features(data_dir = r'../emg_recordings/13-01-2023/', 
                                file_calibration = 'calib_cecile/mydata.csv', 
@@ -25,13 +26,18 @@ torstein.all_features(torstein.normDF, torstein.normDF.shape[0])
 # ax = cecile.normDF.plot.bar(rot = 0)
 # ax = cecile.normDF.plot.bar(x = cecile.labels_list, y = cecile.mavDF, rot = 0)
 
-plotdata = pd.DataFrame({"mav Cécile" : [cecile.mavDF.to_numpy()[0]], 
-                         "mav Torstein" : [torstein.mavDF.to_numpy()[0]]}, index = [str(cecile.labels_list[2:])])
+plotdata = pd.DataFrame({"mav Cécile" : cecile.mavDF.values.tolist()[0], 
+                         "mav Torstein" : torstein.mavDF.values.tolist()[0]}, index = cecile.labels_list[2:])
 plotdata.plot(kind="bar")
+plt.title("Mean absolute value")
+plt.xlabel("Muscles")
+plt.ylabel("mavDF")
 
-cecile.normDF.reset_index().plot(x= str(cecile.labels_list[2:]), y=[torstein.mavDF, cecile.mavDF], kind="bar", rot = 0)
-plt.title("Mince Pie Consumption 18/19")
-plt.xlabel("Family Member")
-plt.ylabel("Pies Consumed")
 
-cecile.mavDF.plot.bar(rot = 0)
+# cecile.normDF.reset_index().plot(x= str(cecile.labels_list[2:]), 
+#                                  y=[torstein.mavDF, cecile.mavDF], 
+#                                  title = "Mean absolute value", 
+#                                  xlabel = "Muscles", 
+#                                  ylabel = "mavDF",
+#                                  kind="bar",
+#                                  rot = 0)
