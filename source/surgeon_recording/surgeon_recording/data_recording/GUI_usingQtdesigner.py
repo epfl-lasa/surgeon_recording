f9dd1420a28c0ebd27e02c803304744ce26bac97
cecilecbs <cecile.cabasse@gmail.com>
2023-07-21 17:41:19 +0200

    Updated GUI_usingQtdesigner

--- source/surgeon_recording/surgeon_recording/data_recording/GUI_usingQtdesigner.py
+++ source/surgeon_recording/surgeon_recording/data_recording/GUI_usingQtdesigner.py
@@ -13,19 +13,60 @@ class MyWindow(QDialog):
         self.initUI() #to initialize the user interface of the window. It is defined in the MyWindow class, 
                       #and it sets up the layout and adds the checkbox widget to the window.
                       
-    def dropdownmenu(self):
-        cb.addItem("Right hand strongly preferred")
-        cb.addItem("Right hand preferred")
+    def dropdownmenu(self, cb):
+        cb.addItem("Always right hand")
+        cb.addItem("Usually right hand")
         cb.addItem("No preference")
-        cb.addItem("Left hand preferred")
-        cb.addItem("Left hand strongly preferred")
+        cb.addItem("Usualy left hand")
         return(cb)
     
-    def Edinburgh_handedness(self, task, cb):
+    def convertDdmToNumber(self, ddm):
+        if ddm == "Always right hand" :
+            self.sum_R = 2 
+        elif ddm == "Usually right hand":
+            self.sum_R +=1  
+        elif ddm == "No preference":
+            self.sum_L += 1
+            self.sum_R += 1
+        elif ddm == "Usualy left hand":
+            self.sum_L += 1
+        elif ddm == "Always left hand":
+            self.sum_L += 2
+        return ( self.sum_L, self.sum_R)
         
-        cb = self.dropdownmenu() 
-        return (ddm)
-                   
+    def Edinburgh_handedness(self):
+        selected_ddm1 = self.ddm1.currentText()
+        selected_ddm2 = self.ddm2.currentText()
+        selected_ddm3 = self.ddm3.currentText()
+        selected_ddm4 = self.ddm4.currentText()
+        selected_ddm5 = self.ddm5.currentText()
+        selected_ddm6 = self.ddm6.currentText()
+        selected_ddm7 = self.ddm7.currentText()
+        selected_ddm8 = self.ddm8.currentText()
+        selected_ddm9 = self.ddm9.currentText()
+        selected_ddm10 = self.ddm10.currentText()
+        
+        self.sum_L = 0
+        self.sum_R = 0
+        
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm1)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm2)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm3)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm4)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm5)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm6)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm7)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm8)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm9)
+        sum_L, sum_R = self.convertDdmToNumber(selected_ddm10)
+        
+        handedness_formula = 100 *((sum_R - sum_L) / (sum_R + sum_L)) #formula to compute handedness
+        print("handedness = ", handedness_formula)
+        return (handedness_formula)
+    
+    def updateHandedness(self):
+        self.handedness = self.Edinburgh_handedness()
+        
     #Function to avoid having the same arm for both tools (tweezers and needle holder)
     def updateNeedleHolder_cb(self): 
         self.needleholder_cb.clear() #clear existing item
@@ -37,8 +78,8 @@ class MyWindow(QDialog):
             self.needleholder_cb.addItem("L")
         
     
-    def on_clicked(self,msg): #save the information and says thanks
-        np.save("S_x_general_info.npy",msg)
+    def on_clicked(self, data): #save the information and says thanks
+        np.save("S_x_general_info.npy", data)
         
         message = QMessageBox()
         # message.setText(msg)
@@ -50,10 +91,8 @@ class MyWindow(QDialog):
         
         #WINDOW PARAMETERS
         loadUi("GUImicrosurgery.ui", self) # Load the .ui file into the MyWindow
-        
-                
+                        
         #WINDODW CONTENT 
-
         sex_list = ["F", "M"]
         self.sex_cb.addItems(sex_list)
         
@@ -74,22 +113,37 @@ class MyWindow(QDialog):
         self.confidenceAnastomosis_cb.addItems(confidenceAnastomosis_list)
     
         #Handedness questionnaire
-        for i in range(1,11):
-            self.ddm + str(i) = self.dropdownmenu( )
-        # label_writing, = self.Edinburgh_handedness("1. Writing", self.ddm1 )
-        # label_drawing, self.ddm2 = self.Edinburgh_handedness("2. Drawing")
-        # label_throwing, self.ddm3 = self.Edinburgh_handedness("3. Throwing")
-        # label_scissors, self.ddm4 = self.Edinburgh_handedness("4. Using scissors")
-        # label_teeth, self.ddm5 = self.Edinburgh_handedness("5. Brushing teeth")
-        # label_knife, self.ddm6 = self.Edinburgh_handedness("6. Using knife (without fork)")
-        # label_spoon, self.ddm7 = self.Edinburgh_handedness("7. Using a spoon")
-        # label_broom, self.ddm8 = self.Edinburgh_handedness("8. Using a broom (dominant hand)")
-        # label_match, self.ddm9 = self.Edinburgh_handedness("9. Striking a match")
-        # label_jar, self.ddm10 = self.Edinburgh_handedness("10. Opening a jar")
+        self.ddm1 = self.dropdownmenu(self.ddm1)
+        self.ddm2 = self.dropdownmenu(self.ddm2)
+        self.ddm3 = self.dropdownmenu(self.ddm3)
+        self.ddm4 = self.dropdownmenu(self.ddm4)
+        self.ddm5 = self.dropdownmenu(self.ddm5)
+        self.ddm6 = self.dropdownmenu(self.ddm6)
+        self.ddm7 = self.dropdownmenu(self.ddm7)
+        self.ddm8 = self.dropdownmenu(self.ddm8)
+        self.ddm9 = self.dropdownmenu(self.ddm9)
+        self.ddm10 = self.dropdownmenu(self.ddm10)
         
+        self.handedness = self.Edinburgh_handedness()
+
+        self.ddm1.currentIndexChanged.connect(self.updateHandedness) # Connect signal to update Handedness method
+        self.ddm2.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm3.currentIndexChanged.connect(self.updateHandedness)
+        self.ddm4.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm5.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm6.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm7.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm8.currentIndexChanged.connect(self.updateHandedness)
+        self.ddm9.currentIndexChanged.connect(self.updateHandedness) 
+        self.ddm10.currentIndexChanged.connect(self.updateHandedness) 
+
+
         #Save data
+        data = self.sex
         self.saveButton.clicked.connect(lambda: self.on_clicked(name.toPlainText()))
         
+        
+
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
